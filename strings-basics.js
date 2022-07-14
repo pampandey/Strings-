15af09c1020ed85b8b30a0f8c3fa8e0e77af2089
Pramila Pandey <pam.pandey@Pramilas-MacBook-Air.local>
2022-07-14 12:01:47 -0400

    Strings File

---
+++ strings-basics.js
@@ -0,0 +1,16 @@
+const message = ' Hello Javascript Students'
+const msgtoLowerCase = message.toLowerCase()
+
+const msgtoUpperCase = message.toUpperCase()
+
+
+
+console.log(message)
+console.log(msgtoUpperCase)
+console.log(msgtoLowerCase)
+
+
+const myschool 'Boca Code'
+myschool.log(Code)
+
+const mystring +
